a1cd347d96e9c54cf06405a848d5fb922bfe7b9c
Marcos Gardinali <100800469+MarcosGardinali@users.noreply.github.com>
2023-10-25 09:56:27 -0300

    New

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Cabecalho from "./Componentes/Cabecalho";
+import Conteudo from "./Componentes/Conteudo";
+
+function App() {
+  return (
+    <div className="App">
+      <Cabecalho />
+      <Conteudo />
+    </div>
+  );
+}
+
+export default App;
