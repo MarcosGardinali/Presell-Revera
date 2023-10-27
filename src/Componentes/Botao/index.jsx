53fb358b11ce18e91a7366fca67b43c32bfa69c4
Marcos Gardinali <100800469+MarcosGardinali@users.noreply.github.com>
2023-10-27 11:56:44 -0300

    .

--- src/Componentes/Botao/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import style from './botao.module.css'
-
-export default function Botao({children}) {
-  return (
-    <a href="https://ev.braip.com/ref?pv=prorg6n7&af=afi7gj37q3" className={style.botao}>{children}</a>
-  )
-}
