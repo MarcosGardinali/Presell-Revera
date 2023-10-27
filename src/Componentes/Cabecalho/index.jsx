b1b78c0016890a6021297dd16fae79c6edab71b8
Marcos Gardinali <100800469+MarcosGardinali@users.noreply.github.com>
2023-10-27 11:57:14 -0300

    New

---
+++ src/Componentes/Cabecalho/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from './cabecalho.module.css'
+
+export default function Cabecalho() {
+  return (
+    <header className={style.cabecalho}>
+       <img src="https://reveracaps.com/wp-content/uploads/2023/05/logo.png" alt="Logo" />     
+    </header>
+  )
+}
