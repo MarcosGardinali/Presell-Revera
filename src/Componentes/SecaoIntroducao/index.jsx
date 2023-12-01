0d6898fb18751c315cd71e8443373aa4647e03a3
Marcos Gardinali <100800469+MarcosGardinali@users.noreply.github.com>
2023-12-01 12:32:54 -0300

    Update

---
+++ src/Componentes/SecaoIntroducao/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from './secaointroducao.module.css'
+
+export default function SecaoIntroducao() {
+  return (
+    <main className={style.conteudo__introducao}>
+        {}
+    </main>
+  )
+}
